3d7bc29e9d50617cdf217357fee9aaeacd9dc4c6
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2025-01-19 20:53:19 +0300

    save

--- src/layout/dashboard/body/recentEmails/index.tsx
+++ src/layout/dashboard/body/recentEmails/index.tsx
@@ -1,4 +1,8 @@
-import { faThumbTack } from '@fortawesome/free-solid-svg-icons';
+import {
+	faFile,
+	faImage,
+	faThumbTack,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PrimaryOutlineBtn } from '../../../../components/buttons';
 const RecentEmails = () => {
@@ -20,12 +24,12 @@ const RecentEmails = () => {
 			body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
 			profileImage:
 				'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=3744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
 			files: [
-				'Master_file.fig',
-				'CoverPreview.jpg',
-				'CoverPreview3.jpg',
-				'Master_file2.fig',
-				'CoverPreview3.jpg',
+				{ name: 'Master_file.fig', type: 'file' },
+				{ name: 'CoverPreview.jpg', type: 'image' },
+				{ name: 'CoverPreview3.jpg', type: 'image' },
+				{ name: 'Master_file2.fig', type: 'file' },
 			],
 			pinned: true,
 		},
@@ -46,7 +50,11 @@ const RecentEmails = () => {
 			body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.',
 			profileImage:
 				'https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?q=80&w=3880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			files: ['file1.fig', 'CoverPreview5.jpg', 'CoverPreview8.jpg'],
+			files: [
+				{ name: 'file1.fig', type: 'file' },
+				{ name: 'CoverPreview5.jpg', type: 'image' },
+				{ name: 'CoverPreview8.jpg', type: 'image' },
+			],
 			pinned: false,
 		},
 	];
@@ -76,14 +84,14 @@ const RecentEmails = () => {
 									<div className='w-ful py-[.9375rem] px-[1.8625rem] flex items-center justify-between gap-[3rem]'>
 										{/* image + data */}
 										<div className='w-full flex items-center'>
-											<div className="w-[5rem] h-[3rem] leading-[3rem] rounded-[50%] text-[1.5rem] overflow-hidden">
+											<div className='w-[5rem] h-[3rem] leading-[3rem] rounded-[50%] text-[1.5rem] overflow-hidden'>
 												<img
 													className='block w-full h-full object-cover'
 													src={email.profileImage}
 													alt={email.name}
 												/>
 											</div>
-											<div className="ml-4">
+											<div className='ml-4'>
 												<h4 className='leading-[1.5] font-[500] text-[1.125rem] text-[var(--text-dark)]'>
 													{email.title}
 												</h4>
@@ -93,7 +101,7 @@ const RecentEmails = () => {
 											</div>
 										</div>
 										{/* pin mark btn */}
-										<div className="max-w-full flex items-center justify-end">
+										<div className='max-w-full flex items-center justify-end'>
 											<button className='w-[3rem] h-[3rem] border-[0.0625rem] border-[var(--primary)] leading-[3rem] text-[var(--primary)] rounded-full flex justify-center items-center text-[1.375rem] hover:bg-[var(--primary)] hover:text-white transition-all duration-300 '>
 												<FontAwesomeIcon
 													icon={faThumbTack}
@@ -101,6 +109,64 @@ const RecentEmails = () => {
 											</button>
 										</div>
 									</div>
+									{email.files.length > 0 && (
+										<div className='flex items-center gap-2 mt-2 px-[1.8625rem]'>
+											{email.files.length > 2
+												? email.files
+														.slice(0, 2)
+														.map((file, index) => (
+															<div
+																key={index}
+																className='py-3 px-9 rounded-full border-[1px] border-[var(--primary)] bg-transparent text-[var(--primary)] text-[0.875rem]'
+															>
+																{file.type ===
+																'image' ? (
+																	<>
+																		<FontAwesomeIcon
+																			className='ml-1'
+																			icon={
+																				faImage
+																			}
+																		/>{' '}
+																		{
+																			file.name
+																		}
+																	</>
+																) : (
+																	<>
+																		<FontAwesomeIcon
+																			className='ml-1'
+																			icon={
+																				faFile
+																			}
+																		/>{' '}
+																		{
+																			file.name
+																		}
+																	</>
+																)}
+															</div>
+														))
+												: email.files.map(
+														(file, index) => (
+															<div
+																key={index}
+																className='py-3 px-9 rounded-full border-[1px] border-[var(--primary)] bg-transparent text-[var(--primary)] text-[0.875rem]'
+															>
+																{file}
+															</div>
+														),
+												  )}
+										</div>
+									)}
+									{email.files.length > 2 && (
+										<div className='py-[.9375rem] px-[1.8625rem]'>
+											<button className='capitalize mt-2 py-3 px-8 rounded-full bg-[var(--rgba-primary-1)] text-[var(--primary)] text-[0.875rem]'>
+												view {email.files.length - 2}{' '}
+												more files
+											</button>
+										</div>
+									)}
 								</div>
 							))}
 					</div>
--- src/layout/dashboard/body/totalClientsComponents/index.tsx
+++ src/layout/dashboard/body/totalClientsComponents/index.tsx
@@ -7,8 +7,8 @@ import Stacked from './Stacked';
 const index = () => {
 	return (
 		<div
-			className='w-full h-full elative transition-all border-0 border-transparent
-    grid grid-cols-12 gap-4 '
+			className='w-full relative transition-all border-0 border-transparent
+    grid grid-cols-12 auto-rows-max gap-4 '
 		>
 			<Card>
 				<Stacked />

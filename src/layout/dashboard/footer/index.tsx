65785d34f3e21b0e2c66b97250e00a31f9cb6d23
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2025-01-23 21:06:22 +0300

    add footer

--- src/layout/dashboard/footer/index.tsx
+++ src/layout/dashboard/footer/index.tsx
@@ -1,7 +1,18 @@
 const Footer = () => {
 	return (
-		<div className='overflow-hidden pl-[--dz-sidebar-width] bg-[--card] text-[0.875rem] bg-slate-500 h-[3.2rem]'>
-			footer
+		<div className='pl-[--dz-sidebar-width]  text-[0.875rem] bg-white h-[3.2rem]'>
+			<div className='text-[#9fa4a6] p-[0.9375rem] mt-[1.875rem]'>
+				<p className='text-center leading-[1.8] m-0'>
+					Copyright © Designed &amp; Developed by{' '}
+					<a
+						href='#'
+						className='text-[var(--primary)]'
+					>
+						Hoda Salah
+					</a>{' '}
+					2025
+				</p>
+			</div>
 		</div>
 	);
 };

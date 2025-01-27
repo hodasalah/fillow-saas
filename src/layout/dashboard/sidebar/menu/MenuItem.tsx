0c56b8f27b79eac0c540ffd014d5dca03dd40d22
Hoda Salah <37372736+hodasalah@users.noreply.github.com>
2025-01-27 12:15:14 +0300

    add active class to first child in sidebar element

--- src/layout/dashboard/sidebar/menu/MenuItem.tsx
+++ src/layout/dashboard/sidebar/menu/MenuItem.tsx
@@ -1,6 +1,6 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router';
 import { useAppSelector } from '../../../../hooks/hooks';
 import './MenuItem.css';
@@ -18,39 +18,43 @@ interface MenuItemProps {
 
 const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
 	const isOpen = useAppSelector((state) => state.sidebar.isOpen);
-	
-		return (
-			<li className='menuItem'>
-				<a
-					className={`has-arrow`}
-					href='javascript:void()'
-				>
-					<FontAwesomeIcon icon={item.icon as IconProp} />
-					<span className=''>{item.name}</span>
-				</a>
-				<ul
-					className='metismenu relative flex flex-col transition-all duration-300 ease-in-out py-2 px-0'
-					id={'submenu'}
-				>
-					{item.hasSubMenu &&
-						item.submenu &&
-						item.submenu.map((submenu) => (
-							<li
-								className={`flex flex-col my-[5px] mx-0 pr-[5px] transition-all duration-300 ease-in-out`}
-								key={submenu}
+	useEffect(() => {
+		if (item.name === 'Dashboard') {
+			ref?.current?.classList?.add('mm-active');
+		}
+	}, []);
+	const ref = useRef<HTMLLIElement>(null);
+	return (
+		<li
+			className='menuItem'
+			ref={ref}
+		>
+			<a className={`has-arrow`}>
+				<FontAwesomeIcon icon={item.icon as IconProp} />
+				<span className=''>{item.name}</span>
+			</a>
+			<ul
+				className='metismenu relative flex flex-col transition-all duration-300 ease-in-out py-2 px-0'
+				id={'submenu'}
+			>
+				{item.hasSubMenu &&
+					item.submenu &&
+					item.submenu.map((submenu) => (
+						<li
+							className={`flex flex-col my-[5px] mx-0 pr-[5px] transition-all duration-300 ease-in-out`}
+							key={submenu}
+						>
+							<NavLink
+								to={`/${submenu.toLowerCase()}`}
+								className={`nav-link `}
 							>
-								<NavLink
-									to={`/${submenu.toLowerCase()}`}
-									className={`nav-link `}
-								>
-									{submenu}
-								</NavLink>
-							</li>
-						))}
-				</ul>
-			</li>
-		);
-	}
-
+								{submenu}
+							</NavLink>
+						</li>
+					))}
+			</ul>
+		</li>
+	);
+};
 
 export default MenuItem;
